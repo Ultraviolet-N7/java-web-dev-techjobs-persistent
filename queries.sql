fe159c39a5ecaae9c2bb420de01577f9bda6df86
Ultraviolet-N7 <meieramberj@gmail.com>
2021-04-02 22:04:01 -0500

    Edited queries.sql a second time

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
-SELECT * FROM techjobs.job;
+Columns:
+id int PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
