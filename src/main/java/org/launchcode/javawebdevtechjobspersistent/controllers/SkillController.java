646ca18d24f719b81a8f3d49d198d96cc3d2a437
Ultraviolet-N7 <meieramberj@gmail.com>
2021-04-03 17:53:17 -0500

    Added jobs field to Employer, updated Job model

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -1,7 +1,8 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Employer;
-import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,47 +13,47 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("employers")
-public class EmployerController {
+@RequestMapping("skills")
+public class SkillController {
 
     @Autowired
-    private EmployerRepository employerRepository;
-
+    private SkillRepository skillRepository;
 
     @GetMapping
     public String displayAllEmployers(Model model) {
-        model.addAttribute("employers", employerRepository.findAll());
-        return "employers/index";
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
     }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
-        model.addAttribute(new Employer());
-        return "employers/add";
+        model.addAttribute(new Skill());
+        return "skills/add";
     }
 
     @PostMapping("add")
-    public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
-                                    Errors errors, Model model) {
+    public String processAddEmployerForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
 
         if (errors.hasErrors()) {
-            return "employers/add";
+            return "skills/add";
         }
 
-        employerRepository.save(newEmployer);
+        skillRepository.save(newSkill);
         return "redirect:";
     }
 
-    @GetMapping("view/{employerId}")
-    public String displayViewEmployer(Model model, @PathVariable int employerId) {
+    @GetMapping("view/{skillId}")
+    public String displayViewEmployer(Model model, @PathVariable int skillId) {
 
-        Optional optEmployer = employerRepository.findById(employerId);
+        Optional optEmployer = skillRepository.findById(skillId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
-            return "employers/view";
+            return "skills/view";
         } else {
             return "redirect:../";
         }
     }
+
 }
